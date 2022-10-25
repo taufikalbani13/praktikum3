8b4363ed82cdccde0c467a271a3f37cd2d247e4f
taufikalbani13 <115517181+taufikalbani13@users.noreply.github.com>
2022-10-25 22:04:40 +0700

    Add files via upload

---
+++ lingkaran.py
@@ -0,0 +1,8 @@
+import math
+r = float(input("Masukan Jari-jari = "))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print("Luas Lingkaran =",format(luas,'.2f'))
+print("Keliling Lingkaran =",format(keliling,'.2f'))
